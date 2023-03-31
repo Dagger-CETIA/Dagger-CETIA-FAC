5741276029424898dd0f1c395a0bc4312e2838c5
hyachaguac <hyachaguac@unal.edu.co>
2023-03-31 11:30:03 -0500

    Update streamWeb.py

--- Cámara/streamWeb.py
+++ Cámara/streamWeb.py
@@ -1,44 +1,39 @@
-import socket, cv2, pickle, struct
-import imutils
+# Using opencv access webcam and transmit the video in HTML
 import cv2
+import  pyshine as ps #  pip3 install pyshine==0.0.9
+HTML="""
+<html>
+<head>
+<title>Dagger</title>
+</head>
 
-
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_name  = socket.gethostname()
-host_ip = '192.168.43.190' # Enter the Drone IP address
-print('HOST IP:',host_ip)
-port = 9999
-socket_address = (host_ip,port)
-server_socket.bind(socket_address)
-server_socket.listen()
-print("Listening at",socket_address)
-
-def start_video_stream():
-	client_socket,addr = server_socket.accept()
-	camera = True
-	if camera == True:
-		vid = cv2.VideoCapture(0)
-	else:
-		vid = cv2.VideoCapture('videos/boat.mp4')
-	try:
-		print('CLIENT {} CONNECTED!'.format(addr))
-		if client_socket:
-			while(vid.isOpened()):
-				img,frame = vid.read()
-
-				frame  = imutils.resize(frame,width=320)
-				a = pickle.dumps(frame)
-				message = struct.pack("Q",len(a))+a
-				client_socket.sendall(message)
-				cv2.imshow("TRANSMITTING TO CACHE SERVER",frame)
-				key = cv2.waitKey(1) & 0xFF
-				if key ==ord('q'):
-					client_socket.close()
-					break
-
-	except Exception as e:
-		print(f"CACHE SERVER {addr} DISCONNECTED")
-		pass
-
-while True:
-	start_video_stream()
+<body>
+<center><h1> Dagger Live Streaming using OpenCV </h1></center>
+<center><img src="stream.mjpg" width='640' height='480' autoplay playsinline></center>
+</body>
+</html>
+"""
+def main():
+    StreamProps = ps.StreamProps
+    StreamProps.set_Page(StreamProps,HTML)
+    address = ('192.168.43.190',9000) # Enter Drone IP address  
+    try:
+        StreamProps.set_Mode(StreamProps,'cv2')
+        capture = cv2.VideoCapture(0)
+        capture.set(cv2.CAP_PROP_BUFFERSIZE,4)
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH,720)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
+        capture.set(cv2.CAP_PROP_FPS,60)
+        StreamProps.set_Capture(StreamProps,capture)
+        StreamProps.set_Quality(StreamProps,90)
+        server = ps.Streamer(address,StreamProps)
+        print('Server started at','http://'+address[0]+':'+str(address[1]))
+        server.serve_forever()
+        
+    except KeyboardInterrupt:
+        capture.release()
+        server.socket.close()
+        
+if __name__=='__main__':
+    main()
+    
