8ece0f7d8ed0baff5f4cc9824514179de066340b
hyachaguac <hyachaguac@unal.edu.co>
2023-04-10 14:28:14 -0500

    Update gstreamerConnectionTest.py

--- Cámara/gstreamerConnectionTest.py
+++ Cámara/gstreamerConnectionTest.py
@@ -1,6 +1,10 @@
 # Pipeline Gstreamer usando una webcam desde la raspberry Pi 4 model B:
 # gst-launch-1.0 -v v4l2src device=/dev/video0 ! videoconvert ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=192.168.43.54 port=9999
 
+# En resolución 2592x1944
+# gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,width=2592,height=1944 ! videoconvert ! x264enc ! rtph264pay ! udpsink host=192.168.43.54 port=9999
+
+
 import cv2
 
 cap = cv2.VideoCapture("udpsrc port=9999 ! application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)JPEG,payload=(int)26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink")
